b95e360610b215b05cc5b0c6ba8f5dc15c48cf57
Khoo Yi-Lin <khooyilin@gmail.com>
2019-02-02 16:49:38 +0800

    Built the Profile section of My Account screen

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { 
 	View, 
 	StyleSheet, 
-	Text 
+	Text,
+  Image 
 } from 'react-native';
 
 import {Icon} from 'react-native-elements';
@@ -26,12 +27,43 @@ export default class AccountScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>My Account</Text>
-        <GreyBox 
-          boxMarginTop= { 20 }
-          boxWidth = { 200 }
-          boxHeight = { 200 }
-        />
+        
+        
+        
+        <View style={[globalStyles.boxStyle, {marginTop: 20, width: 334, height: 168}]}>
+          
+
+          <Text style={{fontFamily: 'Roboto-Bold', color:'#5FB67D', marginLeft: 20, marginTop: 0}}> PROFILE</Text>
+          <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
+            <Image source={require("../assets/images/avatar-penguin.png")} 
+            style={{marginTop: 10, marginLeft: 25}}/>
+
+            <View>
+              <Text style={{fontFamily: 'Roboto-Bold', marginLeft: 0, marginTop: 2}}> Chen Xiao Ming</Text>
+              <Text style={{marginLeft: 0, marginTop: 4}}> ultragreensaver@gmail.com</Text>
+              <Text style={{marginLeft: 0, marginTop: 5}}> +65 96250796 </Text>
+
+              <View style = {{flexDirection: 'row', marginLeft: 0, marginTop: 10, justifyContent:'space-between'}}>
+                
+                <GreenButton
+                  text="Edit Details"  
+                  onPress={() => null}
+                  buttonWidth= { 100 }
+                  buttonHeight= { 40 }
+                  buttonMarginTop= { 0 }
+                  buttonMarginLeft= { 0 }
+                />
+
+                <Text style={{fontFamily: 'Roboto-Light', marginTop: 10}}> Log Out </Text>
+            </View>
+          </View>
+        
+
+        </View> 
+        </View>
+
+        
+            
       </View>
     );
   }
